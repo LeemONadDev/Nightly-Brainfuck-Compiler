ede9e47cd34b5cb82a5be1437e09fb064a9cab26
LeemONadDev <hackermix101@gmail.com>
2023-10-23 21:24:24 +0300

    Create brainfuck_compiler_additional.py
    Library for Nightly Brainfuck Compiler

---
+++ brainfuck_compiler_additional.py
@@ -0,0 +1,20 @@
+def lib_func_load(lib_path):
+    #functions load
+    lib_functions = {}
+    if lib_path != "":
+        f = open(lib_path)
+        lib_functions = {}
+        lib_func_load_proc = True
+        while lib_func_load_proc:
+            current_line = f.readline()
+            if "def_func" in current_line:
+                current_line_temp = current_line.split()
+                lib_functions[current_line_temp[1]] = current_line_temp[2]
+            elif "$" in current_line:
+                pass
+            
+            if not current_line:
+                lib_func_load_proc = False
+    else:
+        pass
+    return lib_functions
